a55b39caf088b2c575d709253b635968e6254b25
Dino <dleootta@gmail.com>
2025-08-03 16:39:11 +0200

    Now the operate() is worki,g. Needs to fix AC - Need to put the result to firstNum

--- script.js
+++ script.js
@@ -37,28 +37,39 @@ show.appendChild(display);
 
 const buttons = document.querySelectorAll(".digit");
 
+// Variable working with AC button
 const erase = document.querySelector(".erase");
 erase.addEventListener("click", () => {
   data = ``;
   display.textContent = data;
 });
 
+// Function showing numbers/operators in the display
+function showDisplay() {
+  if (operateur !== null) {
+    display.textContent = firstNum + operateur + data;
+  } else {
+    display.textContent = data;
+  }
+}
+
+// Clicking on buttons make them works
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
     data += button.textContent;
-    display.textContent = data;
+    showDisplay();
     console.log(data);
   })
 );
 
+// Clicking on operators make them works
 const operators = document.querySelectorAll(".operator");
-
 operators.forEach((operator) =>
   operator.addEventListener("click", () => {
     operateur = operator.textContent;
-    // display.textContent = data + operateur;
     firstNum = data;
     data = ``;
+    showDisplay();
     console.log(operateur);
     console.log(firstNum);
   })
@@ -68,5 +79,10 @@ const equal = document.querySelector(".equal");
 
 equal.addEventListener("click", () => {
   secondNum = data;
+  firstNum = Number(firstNum);
+  secondNum = Number(secondNum);
   console.log(secondNum);
+  let result = operate(operateur, firstNum, secondNum);
+  display.textContent = result;
+  data = result;
 });
